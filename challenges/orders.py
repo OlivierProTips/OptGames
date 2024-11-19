417a5be4ffedf0dcb4704d4b9dd7451f01dbad13
OlivierProTips <olivierprotips@gmail.com>
2024-11-19 11:03:53 +0100

    External orders.py

---
+++ challenges/orders.py
@@ -0,0 +1,16 @@
+class ChallengeOrder:
+    def __init__(self, title, order):
+        self.title = title
+        self.order = order
+        
+orders = [
+    ChallengeOrder("Hidden somewhere",      1),
+    ChallengeOrder("Six Fo(u)r the Base",   2),
+    ChallengeOrder("Too Many Bits",         3),
+    ChallengeOrder("Unbelievable",          4),
+    ChallengeOrder("Cattle",                5),
+    ChallengeOrder("Odobenus",              6),
+    ChallengeOrder("TXT Message",           7),
+    ChallengeOrder("Zulu",                  8),
+    ChallengeOrder("MatryoshkaQR",          9),
+]
--- update_order.py
+++ update_order.py
@@ -1,23 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
 import os
-
-class ChallengeOrder:
-    def __init__(self, title, order):
-        self.title = title
-        self.order = order
-                
-orders = [
-    ChallengeOrder("Hidden somewhere", 1),
-    ChallengeOrder("Six Fo(u)r the Base", 2),
-    ChallengeOrder("Too Many Bits", 3),
-    ChallengeOrder("Unbelievable", 4),
-    ChallengeOrder("Cattle", 5),
-    ChallengeOrder("Odobenus", 6),
-    ChallengeOrder("TXT Message", 7),
-    ChallengeOrder("Zulu", 8),
-    ChallengeOrder("MatryoshkaQR", 9),
-]
+import challenges.orders as orders
 
 # Configuration de la base de données (ajustez le chemin si nécessaire)
 BASE_DIR = "app/game.db"
