6236386fd4d3b20ae6f4a1e5e5837e1b06c049d0
OlivierProTips <olivierprotips@gmail.com>
2024-11-25 14:14:06 +0100

    Add docker_ports

--- admin/clear_users.py
+++ admin/clear_users.py
@@ -12,6 +12,7 @@ metadata.reflect(bind=engine)
 # Accéder aux tables
 results_table = Table('results', metadata, autoload_with=engine)
 users_table = Table('users', metadata, autoload_with=engine)
+dockers_table = Table('docker_ports', metadata, autoload_with=engine)
 
 # Connexion à la base de données
 with engine.connect() as connection:
@@ -22,5 +23,9 @@ with engine.connect() as connection:
     # Vider la table users
     connection.execute(users_table.delete())
     print("Table 'users' vidée avec succès.")
+    
+    # Vider la table docker_ports
+    connection.execute(dockers_table.delete())
+    print("Table 'docker_ports' vidée avec succès.")
     
     connection.commit()
